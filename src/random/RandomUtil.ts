9d0d1a3e8a99c84325188d7b73da8d1ca801005c
Martti Marran <martti.marran@hotmail.com>
2022-03-02 14:36:41 +0200

    Fix lint errors

--- src/random/RandomUtil.ts
+++ src/random/RandomUtil.ts
@@ -1,5 +1,5 @@
 import BigInteger from "big-integer";
 
 export const pseudoRandomLong = (): BigInteger.BigInteger => {
-  return BigInteger.randBetween(0, 9223372036854775807);
+  return BigInteger.randBetween(0, "9223372036854775807");
 };
